70ab2e954931dbdf7496ef6fa5c41475229b7d6d
dilan-schintha97 <dil97an@gmail.com>
2023-09-20 17:09:22 +0530

    added react router dom

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer"
+import NavBar from "../components/NavBar"
+
+
+const Home = () => {
+  return (
+    <div>
+        <NavBar/>
+         body
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
